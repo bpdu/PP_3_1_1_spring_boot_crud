de85408265f330113a486d5b3ac3df610f712eae
Alexey Rumyantsev <me@bpdu.ru>
2022-07-10 08:03:37 +0300

    Initial commit

---
+++ src/main/java/ru/bpdu/PP_3_1_1_spring_boot_crud/Pp311SpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package ru.bpdu.PP_3_1_1_spring_boot_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp311SpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp311SpringBootCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bpdu/PP_3_1_1_spring_boot_crud/Pp311SpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bpdu.PP_3_1_1_spring_boot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp311SpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
