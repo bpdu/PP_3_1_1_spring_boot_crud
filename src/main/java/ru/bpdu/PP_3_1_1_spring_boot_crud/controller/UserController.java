10ae27931dada6a7c685def6ef0900b19e8e0292
alexr <me@bpdu.ru>
2022-07-11 14:54:27 +0300

    Change UserController

--- src/main/java/ru/bpdu/PP_3_1_1_spring_boot_crud/controller/UserController.java
+++ src/main/java/ru/bpdu/PP_3_1_1_spring_boot_crud/controller/UserController.java
@@ -1,49 +1,52 @@
 package ru.bpdu.PP_3_1_1_spring_boot_crud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.bpdu.PP_3_1_1_spring_boot_crud.entity.User;
 import ru.bpdu.PP_3_1_1_spring_boot_crud.service.UserService;
 
-@RestController
-@RequestMapping(value="/users")
+@Controller
+@RequestMapping(value = "/user")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     public UserController(UserService userService) { this.userService = userService; }
 
-    @GetMapping(value="findAll")
+    @GetMapping(value="/list")
     public String showUsers(Model model) {
         model.addAttribute("users", userService.getUserList());
         return("users");
     }
 
-    @GetMapping(value="create")
+    @GetMapping(value="/create")
     public String createUserRedirect(User user) {
         return "create";
     }
 
-    @PostMapping(value="create")
+    @PostMapping(value="/create")
     public String createUser(User user) {
         userService.addUser(user);
         return "redirect:/users";
     }
 
-    @GetMapping(value = "delete/{id}")
+    @GetMapping(value = "/delete/{id}")
     public String deleteUser(@PathVariable("id") long id) {
         userService.deleteUser(id);
         return("redirect:/users");
     }
 
-    @GetMapping(value="update/{id}")
+    @GetMapping(value="/update/{id}")
     public String updateUserRedirect(@PathVariable("id") long id, Model model) {
         User user = userService.getUser(id);
         model.addAttribute("user", user);
         return "update";
     }
 
-    @PostMapping(value="update")
+    @PostMapping(value="/update")
     public String updateUser(User user) {
         userService.updateUser(user.getId(), user.getName(), user.getEmail(), user.getAge());
         return "redirect:/users";
